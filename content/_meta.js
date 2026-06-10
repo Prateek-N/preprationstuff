7eaa62b062262b7b9b0a671ca11c065b5ef4f41e
Prateek Narvariya <prateekn.silverspace@gmail.com>
2026-06-10 17:28:16 -0400

    feat(docs): add vaishnavi torc prep guide

--- content/_meta.js
+++ content/_meta.js
@@ -2,5 +2,6 @@ export default {
   index: 'Docs Home',
   'rivian-vw-ota-interview-prep': 'Rivian/VW OTA Interview Prep Guide',
   'abbott-onsite-prep-guide': 'Abbott Onsite Prep Guide',
-  'tcs-sandeep': 'TCS Sandeep Prep Guide'
+  'tcs-sandeep': 'TCS Sandeep Prep Guide',
+  'vaishnavi-torc-mcu-applications-prep': 'Vaishnavi Torc MCU Applications Prep'
 }
