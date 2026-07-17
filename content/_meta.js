5111590fb664281aa9a1c63e05e5898c2a448fa1
Prateek Narvariya <prateekn.silverspace@gmail.com>
2026-07-17 12:53:12 -0400

    docs: archive unused prep guides into collapsible sub-folder

--- content/_meta.js
+++ content/_meta.js
@@ -1,11 +1,9 @@
 export default {
   index: 'Docs Home',
   'qualcomm-prep-material': 'Qualcomm NPU / Embedded Platform Prep',
-  'rivian-vw-ota-interview-prep': 'Rivian/VW OTA Interview Prep Guide',
   'natera-ai-solutions-prep': 'Natera AI Solutions Prep',
   'torc-prep-guide': 'Torc Prep Guide (Device Drivers)',
-  'abbott-onsite-prep-guide': 'Abbott Onsite Prep Guide',
-  'tcs-sandeep': 'TCS Sandeep Prep Guide',
   'vaishnavi-torc-mcu-applications-prep': 'Vaishnavi Torc MCU Applications Prep',
-  'harnish-frontend-backend-coding-prep': 'Harnish Frontend + Backend Coding Prep'
+  'harnish-frontend-backend-coding-prep': 'Harnish Frontend + Backend Coding Prep',
+  'archived': 'Archived Guides'
 }
---
+++ content/archived/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  'rivian-vw-ota-interview-prep': 'Rivian/VW OTA Interview Prep Guide',
+  'abbott-onsite-prep-guide': 'Abbott Onsite Prep Guide',
+  'tcs-sandeep': 'TCS Sandeep Prep Guide'
+}
